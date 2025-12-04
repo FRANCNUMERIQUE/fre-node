f368fe19bde111957b00bb5f3b348e394e111649
FRANC NUMÉRIQUE FRE <contact.francfre@gmail.com>
2025-12-04 23:44:06 +0100

    Update network.py

--- network.py
+++ network.py
@@ -1,20 +1,13 @@
-import socket
-import json
-
 class NodeNetwork:
-
     def __init__(self):
-        self.nodes = []      # liste des autres nœuds
         self.buffer = []
 
     def collect_transactions(self):
         return self.buffer
 
-    def broadcast_block(self, block):
-        payload = json.dumps(block.to_dict())
-        # envoi TCP ou websocket
-        pass
+    def validators(self):
+        return ["node1", "node2", "node3"]
 
     def collect_signatures(self, block):
-        # reçoit signatures des pairs
-        return []
+        return ["signature1", "signature2"]
+
