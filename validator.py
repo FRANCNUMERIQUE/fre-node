65ef4723dc1df3cd2d70fa83e1aaf7087b4773aa
FRANC NUMÉRIQUE FRE <contact.francfre@gmail.com>
2025-12-04 23:43:45 +0100

    Update validator.py

--- validator.py
+++ validator.py
@@ -1,7 +1,7 @@
 class Validator:
     def validate(self, tx):
-        # Vérification minimale L2 (à enrichir)
         return True
 
     def validate_all(self, txs):
         return [tx for tx in txs if self.validate(tx)]
+
