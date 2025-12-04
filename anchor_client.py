6ffb13f5909c5fd68f67c5c2b68fde740dc8e862
FRANC NUMÉRIQUE FRE <contact.francfre@gmail.com>
2025-12-04 22:53:54 +0100

    Create anchor_client.py

---
+++ anchor_client.py
@@ -0,0 +1,6 @@
+class AnchorClient:
+
+    def push_block(self, block):
+        print(f"[ANCHOR] Envoi du bloc {block['index']} à L1...")
+        # Appel HTTP vers contrat TACT (à implémenter)
+        pass
