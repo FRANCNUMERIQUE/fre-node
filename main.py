84ad18ff9ff37168d83d149e738b870da206a04a
FRANC NUMÉRIQUE FRE <contact.francfre@gmail.com>
2025-12-05 19:11:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
+import time
 from fre_node.validator import Validator
-from fre_node.network import NodeNetwork
+from fre_node.network import Network
+from fre_node.consensus import Consensus
 from fre_node.ledger import Ledger
 from fre_node.block_builder import BlockBuilder
 from fre_node.anchor_client import AnchorClient
-from fre_node.consensus import Consensus
-from fre_node.config import *
+from fre_node.config import NODE_NAME
 
-import time
 
-print(f"[FRE-NODE] Démarrage du nœud : {NODE_ID}")
+class Node:
+    """Nœud FRE Layer 2"""
 
-ledger = Ledger()
-network = NodeNetwork()
-validator = Validator()
-builder = BlockBuilder(ledger)
-anchor = AnchorClient()
-consensus = Consensus(network, ledger, anchor)
+    def __init__(self):
+        self.name = NODE_NAME
+        self.network = Network(self.name)
+        self.ledger = Ledger()
+        self.validator = Validator(self.ledger)
+        self.consensus = Consensus(self.validator, self.network)
+        self.block_builder = BlockBuilder(self.ledger)
+        self.anchor_client = AnchorClient()
 
-while True:
-    txs = network.collect_transactions()
-    valid_txs = validator.validate_all(txs)
-    block = builder.build_block(valid_txs)
+    def start(self):
+        print(f"[FRE-NODE] Démarrage du nœud : {self.name}")
 
-    if consensus.reach(block):
-        builder.finalize_block(block)
-        anchor.push_block(block)
+        while True:
+            # Construire un bloc simulé
+            block = self.block_builder.build_block()
 
-    time.sleep(2)
+            # Ajouter le bloc au ledger
+            self.ledger.add_block(block)
+
+            # Émettre le bloc sur le réseau
+            self.network.broadcast_block(block)
+
+            # Essayer d’ancrer un bloc (simulé)
+            self.anchor_client.send_anchor(block)
+
+            time.sleep(2)
+
+
+if __name__ == "__main__":
+    node = Node()
+    node.start()
